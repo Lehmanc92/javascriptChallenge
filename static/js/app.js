5d6984141a25945fb01914569a530d3667902149
Christian L <Lehmanc92@gmail.com>
2021-06-30 23:00:40 -0400

    Basic done

---
+++ static/js/app.js
@@ -0,0 +1,40 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// Defining function to render new data to table
+
+function tableDraw(data) {
+    data.forEach((data) => {
+        var row = tbody.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+
+    });
+};
+
+tableDraw(tableData);
+
+// Function to filter table
+
+function filterData() {
+    d3.event.preventDefault();
+    var inputSelect = d3.select("input");
+    var inputValue = inputSelect.property("value");
+    var newData = tableData.filter((row) => row.datetime === inputValue);
+    tbody.html("");
+    tableDraw(newData);
+};
+
+// filterData();
+
+var button = d3.select("#filter-btn");
+
+var form = d3.select("#form");
+
+button.on("click", filterData);
+form.on("submit", filterData);
